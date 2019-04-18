54d5761cb69624359bcb0b85200f0b62afaf2c82
Matheus Giovani <teusgiovani@gmail.com>
2019-04-18 01:30:22 -0300

    Update database.js

--- server/socket/core/database.js
+++ server/socket/core/database.js
@@ -21,7 +21,7 @@ class BotDatabase {
 		this.Members 				= this.sequelize.define("member", {
 			id: 					{
 				type: 				Sequelize.INTEGER.UNSIGNED,
-				primaryKey: 		false
+				primaryKey: 		true
 			},
 			uid: 					{
 				type: 				Sequelize.INTEGER.UNSIGNED
